75c71aef647265306635cb6369409c4294ced7a8
jpmairinque <jpmairinque@gmail.com>
2025-03-25 20:49:39 -0300

    add user input

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from functions import *
 
 if __name__ == "__main__":    
-    
-    arr = [3, 1, 8, 2, 5, 7, 4, 6]
-    min_val, max_val = max_min_select(arr, 0, len(arr) - 1)
-    print(f"Menor elemento: {min_val}, Maior elemento: {max_val}")
+    try:
+        arr = list(map(int, input("Digite uma série de números separados por espaço: ").split()))
+        if not arr:
+            raise ValueError("A lista não pode estar vazia.")
+        min_val, max_val = max_min_select(arr, 0, len(arr) - 1)
+        print(f"Menor elemento: {min_val}, Maior elemento: {max_val}")
+    except ValueError as e:
+        print(f"Erro de valor: {e}")
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
